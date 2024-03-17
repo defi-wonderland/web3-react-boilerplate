0423a756dc513e01c3b3c8641124646288de9501
titix <titi@defi.sucks>
2024-03-17 11:04:43 -0300

    fix: test type assertion | build type assertion fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,35 +2,32 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "@typescript-eslint"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint'],
   settings: {
     react: {
-      version: "detect", // Automatically detect the React version
+      version: 'detect', // Automatically detect the React version
     },
   },
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     // Additional rules can be added here
   },
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "react/prop-types": "off", // Turn off prop-types rule for TypeScript files
+        'react/prop-types': 'off', // Turn off prop-types rule for TypeScript files
       },
     },
   ],
--- src/tests/components/App.test.tsx
+++ src/tests/components/App.test.tsx
@@ -1,5 +1,7 @@
 import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { mockRouter } from '../__mocks__';
+import { Router } from 'next/router';
 import MyApp from '~/pages/_app';
 
 vi.mock('../config', () => ({
@@ -7,7 +9,7 @@ vi.mock('../config', () => ({
 }));
 
 vi.mock('~/components', () => ({
-  Themable: ({ children }) => <div>{children}</div>,
+  Themable: ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
   customTheme: {},
 }));
 
@@ -16,7 +18,7 @@ vi.mock('~/hooks', () => ({
 }));
 
 vi.mock('@rainbow-me/rainbowkit', () => ({
-  RainbowKitProvider: ({ children }) => <div>{children}</div>,
+  RainbowKitProvider: ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
   connectorsForWallets: () => ({}),
   injectedWallet: {},
   rainbowWallet: {},
@@ -27,11 +29,11 @@ vi.mock('@rainbow-me/rainbowkit', () => ({
 
 vi.mock('@tanstack/react-query', () => ({
   QueryClient: class {},
-  QueryClientProvider: ({ children }) => <div>{children}</div>,
+  QueryClientProvider: ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
 }));
 
 vi.mock('wagmi', () => ({
-  WagmiProvider: ({ children }) => <div>{children}</div>,
+  WagmiProvider: ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
   createConfig: () => ({}),
   http: () => ({}),
 }));
@@ -43,7 +45,13 @@ vi.mock('wagmi/chains', () => ({
 describe('MyApp', () => {
   it('renders without crashing and checks specific functionality', async () => {
     render(
-      <MyApp Component={({ children }) => <div data-testid='boilerplate-title'>{children}</div>} pageProps={{}} />,
+      <MyApp
+        Component={({ children }: { children: React.ReactNode }) => (
+          <div data-testid='boilerplate-title'>{children}</div>
+        )}
+        pageProps={{}}
+        router={mockRouter as unknown as Router}
+      />,
     );
 
     expect(screen.queryByTestId('boilerplate-title')).not.toBeNull();
