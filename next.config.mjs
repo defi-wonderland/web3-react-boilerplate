a77794c01eb09d00167ff7b2882557664a9d21ec
titix <titi@defi.sucks>
2024-03-18 09:47:23 -0300

    fix: next.config default

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
-};
+const nextConfig = {};
 
 export default nextConfig;
