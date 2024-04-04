7493046924de6f9df71697df242ea2df004c01b3
titix <titi@defi.sucks>
2024-04-04 12:09:59 -0300

    fix: env vars as string type error on building

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,13 +5,9 @@ export const getEnv = (): Env => {
   const NEXT_PUBLIC_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
   const NEXT_PUBLIC_ALCHEMY_KEY = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
 
-  if (!NEXT_PUBLIC_RPC_URL || !NEXT_PUBLIC_PROJECT_ID || !NEXT_PUBLIC_ALCHEMY_KEY) {
-    throw new Error('Environment configuration is missing critical values');
-  }
-
   return {
-    RPC_URL: NEXT_PUBLIC_RPC_URL || '',
-    PROJECT_ID: NEXT_PUBLIC_PROJECT_ID || '',
-    ALCHEMY_KEY: NEXT_PUBLIC_ALCHEMY_KEY || '',
+    RPC_URL: NEXT_PUBLIC_RPC_URL as string,
+    PROJECT_ID: NEXT_PUBLIC_PROJECT_ID as string,
+    ALCHEMY_KEY: NEXT_PUBLIC_ALCHEMY_KEY as string,
   };
 };
