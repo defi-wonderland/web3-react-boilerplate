053a084edbd0e2c20c858bab59d79f4c25c0af93
titix <titi@defi.sucks>
2024-04-04 11:54:51 -0300

    fix: env vars as string type error on building

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,8 +4,8 @@ export const getEnv = (): Env => {
   const { NEXT_PUBLIC_RPC_URL, NEXT_PUBLIC_PROJECT_ID, NEXT_PUBLIC_ALCHEMY_KEY } = process.env;
 
   return {
-    RPC_URL: NEXT_PUBLIC_RPC_URL || '',
-    PROJECT_ID: NEXT_PUBLIC_PROJECT_ID || '',
-    ALCHEMY_KEY: NEXT_PUBLIC_ALCHEMY_KEY || '',
+    RPC_URL: NEXT_PUBLIC_RPC_URL as string,
+    PROJECT_ID: NEXT_PUBLIC_PROJECT_ID as string,
+    ALCHEMY_KEY: NEXT_PUBLIC_ALCHEMY_KEY as string,
   };
 };
